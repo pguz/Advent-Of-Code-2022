4a6acc17f40795c48e041a80cce8e4e476b7fd41
Paweł Guz <pawelguz@gmail.com>
2022-12-25 21:05:32 +0100

    Day 25

--- solutions/day_25.py
+++ solutions/day_25.py
@@ -1,9 +1,42 @@
-# Day 25:
+# Day 25: Full of Hot Air
+
+import pandas as pd
 
 
 def parse_file(fd):
-    return None
+    return (pd.read_csv(fd, header=None).squeeze("columns"),)
 
 
-solution_function_01 = None
+def _from_snafu(snafu):
+    con = {
+        "=": -2,
+        "-": -1,
+        "0": 0,
+        "1": 1,
+        "2": 2,
+    }
+    return sum([con[v] * pow(5, i) for i, v in enumerate(snafu[::-1])])
+
+
+def _to_snafu(number):
+    con = {
+        0: "0",
+        1: "1",
+        2: "2",
+        3: "=",
+        4: "-",
+    }
+    snafu = ""
+    while number:
+        q, r = divmod(number, 5)
+        snafu = con[r] + snafu
+        number = q + 1 if r > 2 else q
+    return snafu
+
+
+def calc_snafu(snafu_numbers):
+    return _to_snafu(snafu_numbers.map(_from_snafu).sum())
+
+
+solution_function_01 = calc_snafu
 solution_function_02 = None
